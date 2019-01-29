51c3558aca37530b0ae9293a70c4b94b429fb404
Štěpán Vraný <vrany.stepan@gmail.com>
2019-01-29 16:15:53 +0100

    Update helm_deploy.sh

--- scripts/helm_deploy.sh
+++ scripts/helm_deploy.sh
@@ -5,7 +5,6 @@ echo "upgrading release ${IMAGE_NAME} in namespace ${GCLOUD_GKE_NAMESPACE} ..."
 echo "APP_NAME: ${APP_NAME}"
 echo "PROJECT_NAME: ${PROJECT_NAME}"
 echo "CI_ENVIRONMENT_NAME: ${CI_ENVIRONMENT_NAME}"
-echo "remote image name: eu.gcr.io/${GCLOUD_PROJECT_ID}/${PROJECT_NAME}/${APP_NAME}"
 echo "IMAGE_TAG: ${IMAGE_TAG}"
 echo "CI_COMMIT_SHORT_SHA: ${CI_COMMIT_SHORT_SHA}"
 echo "CI_COMMIT_REF_NAME: ${CI_COMMIT_REF_NAME}"
@@ -14,6 +13,10 @@ echo "GCLOUD_PROJECT_ID: ${GCLOUD_PROJECT_ID}"
 echo "GCLOUD_GKE_NAMESPACE: ${GCLOUD_GKE_NAMESPACE}"
 echo "IMAGE_NAME: ${IMAGE_NAME}"
 echo "HELM_CHART_PATH: ${HELM_CHART_PATH}"
+echo "--------------------------------------------------------------"
+echo "remote image name: eu.gcr.io/${GCLOUD_PROJECT_ID}/${PROJECT_NAME}/${APP_NAME}"
+echo "--> eu.gcr.io/${GCLOUD_PROJECT_ID}/${PROJECT_NAME}/${APP_NAME}:${CI_COMMIT_SHORT_SHA}"
+echo "--------------------------------------------------------------"
 
 helm upgrade \
   --install \
