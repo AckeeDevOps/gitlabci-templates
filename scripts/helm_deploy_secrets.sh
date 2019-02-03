3e1196d85f269f1f95654db6db2284fa68388222
Štěpán Vraný <vrany.stepan@gmail.com>
2019-02-03 13:18:41 +0100

    Update helm_deploy_secrets.sh

--- scripts/helm_deploy_secrets.sh
+++ scripts/helm_deploy_secrets.sh
@@ -22,19 +22,19 @@ echo "Helm release: ${PROJECT_NAME}-${APP_NAME}-${CI_ENVIRONMENT_NAME}"
 echo "--------------------------------------------------------------"
 
 helm upgrade \
-  --install \
-  -f ${HELM_BASE_VALUES} \
-  -f ${PLUGIN_SECRET_OUTPUT_PATH} \
-  --set general.appName=${APP_NAME} \
-  --set general.projectName=${PROJECT_NAME} \
-  --set general.environment=${CI_ENVIRONMENT_NAME} \
-  --set general.imageName=eu.gcr.io/${GCLOUD_PROJECT_ID}/${PROJECT_NAME}/${APP_NAME} \
-  --set general.imageTag=${IMAGE_TAG} \
-  --set general.meta.buildHash=${CI_COMMIT_SHORT_SHA} \
-  --set general.meta.branch=${CI_COMMIT_REF_NAME} \
-  --set general.meta.repositoryUrl=${CI_PROJECT_URL} \
-  --set general.gcpProjectId=${GCLOUD_PROJECT_ID} \
-  --namespace=${GCLOUD_GKE_NAMESPACE} \
-  --dry-run=false \
-  ${PROJECT_NAME}-${APP_NAME}-${CI_ENVIRONMENT_NAME} \
-  ${HELM_CHART_PATH}
+--install \
+-f ${HELM_BASE_VALUES} \
+-f ${PLUGIN_SECRET_OUTPUT_PATH} \
+--set general.appName=${APP_NAME} \
+--set general.projectName=${PROJECT_NAME} \
+--set general.environment=${CI_ENVIRONMENT_NAME} \
+--set general.imageName=eu.gcr.io/${GCLOUD_PROJECT_ID}/${PROJECT_NAME}/${APP_NAME} \
+--set general.imageTag=${IMAGE_TAG} \
+--set general.meta.buildHash=${CI_COMMIT_SHORT_SHA} \
+--set general.meta.branch=${CI_COMMIT_REF_NAME} \
+--set general.meta.repositoryUrl=${CI_PROJECT_URL} \
+--set general.gcpProjectId=${GCLOUD_PROJECT_ID} \
+--namespace=${GCLOUD_GKE_NAMESPACE} \
+--dry-run=false \
+${PROJECT_NAME}-${APP_NAME}-${CI_ENVIRONMENT_NAME} \
+${HELM_CHART_PATH}
