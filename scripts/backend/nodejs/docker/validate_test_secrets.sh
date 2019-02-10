c40e66866164fa17fe2ae3dc1cef3514519db6b3
Stepan Vrany <stepan.vrany@ackee.cz>
2019-02-10 16:34:31 +0100

    check vault token lenghth

--- scripts/backend/nodejs/docker/validate_test_secrets.sh
+++ scripts/backend/nodejs/docker/validate_test_secrets.sh
@@ -8,12 +8,20 @@ echo "Running validation sequence for CI test with secrets ..."
 [ -z "$VAULTIER_RUN_CAUSE" ] && { echo "VAULTIER_RUN_CAUSE is required"; exit 1; }
 [ -z "$VAULTIER_OUTPUT_FORMAT" ] && { echo "VAULTIER_OUTPUT_FORMAT is required"; exit 1; }
 [ -z "$VAULTIER_SECRET_OUTPUT_PATH" ] && { echo "VAULTIER_SECRET_OUTPUT_PATH is required"; exit 1; }
+[ -z "$VAULTIER_VAULT_ADDR" ] && { echo "VAULTIER_VAULT_ADDR is required"; exit 1; }
+[ -z "$VAULTIER_VAULT_TOKEN" ] && { echo "VAULTIER_VAULT_TOKEN is required"; exit 1; }
 [ -z "$SSH_KEY" ] && { echo "SSH_KEY is required"; exit 1; }
 
 # Perform more sophisticated tests
 echo ${SSH_KEY} | base64 -d | openssl rsa -noout > /dev/null 2>&1
 if [ $? -ne 0 ]; then
   echo "SSH_KEY is broken. Make sure it's base64 encoded RSA private key"
+  exit 1
+fi
+
+token_size=$(echo ${VAULTIER_VAULT_TOKEN} | wc -c)
+if [ $token_size -ne 27 ]; then
+  echo "VAULTIER_VAULT_TOKEN should have exactly 27 characters"
   exit 1
 fi
 
