f93ad43a5ccd45395b760d95428f514d4fe810bd
Stepan Vrany <stepan.vrany@ackee.cz>
2019-02-10 15:48:16 +0100

    add RSA key validation

--- scripts/backend/nodejs/docker/validate_test_secrets.sh
+++ scripts/backend/nodejs/docker/validate_test_secrets.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
 echo "Running validation sequence for CI test with secrets ..."
-# tbd
+
+# Check if variables exist
+[ -z "$VAULTIER_BRANCH" ] && { echo "VAULTIER_BRANCH is required"; exit 1; }
+[ -z "$VAULTIER_SECRET_SPECS_PATH" ] && { echo "VAULTIER_SECRET_SPECS_PATH is required"; exit 1; }
+[ -z "$VAULTIER_RUN_CAUSE" ] && { echo "VAULTIER_RUN_CAUSE is required"; exit 1; }
+[ -z "$VAULTIER_OUTPUT_FORMAT" ] && { echo "VAULTIER_OUTPUT_FORMAT is required"; exit 1; }
+[ -z "$VAULTIER_SECRET_OUTPUT_PATH" ] && { echo "VAULTIER_SECRET_OUTPUT_PATH is required"; exit 1; }
+[ -z "$SSH_KEY" ] && { echo "SSH_KEY is required"; exit 1; }
+
+# Perform more sophisticated tests
+echo ${SSH_KEY} | base64 -d | openssl rsa -noout > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "SSH_KEY is broken. Make sure it's base64 encoded RSA private key"
+  exit 1
+fi
