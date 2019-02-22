75bcfa8088970974484348148576143732b439b8
Stepan Vrany <stepan.vrany@ackee.cz>
2019-02-22 08:58:50 +0100

    change rclone installation method

--- scripts/backend/nodejs/common/rclone-install.sh
+++ scripts/backend/nodejs/common/rclone-install.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-apt-get update -y > /dev/null 2>&1
-apt-get install rclone -y > /dev/null 2>&1
+# Download rclone
+wget https://github.com/ncw/rclone/releases/download/v1.45/rclone-v1.45-linux-386.zip -O rclone.zip > /dev/null 2>&1
+
+# extract rclone to 'rclone' directory
+mkdir -p rclone
+unzip -j -d rclone rclone.zip > /dev/null 2>&1
+
+# install rclone
+mv rclone/rclone /usr/local/bin
+
+# cleanup
+rm -rf rclone rclone.zip
