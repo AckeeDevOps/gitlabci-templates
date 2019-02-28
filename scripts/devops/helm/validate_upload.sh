c7a1d251a027111ff631ba8170f6d1a5cc96756b
Štěpán Vraný <stepan.vrany@ackee.cz>
2019-02-28 08:35:24 +0100

    Update validate_upload.sh

--- scripts/devops/helm/validate_upload.sh
+++ scripts/devops/helm/validate_upload.sh
@@ -1,1 +1,29 @@
 #!/bin/sh
+
+# HELM_REPOSITORY_URL
+# HELM_REPOSITORY_NAME
+# HELM_REPOSITORY_USERNAME
+# HELM_REPOSITORY_PASSWORD
+# HELM_BINARIES_URL
+
+echo "Running validation sequence for Upload Helm chart to repository ..."
+
+if [ "$DEBUG_MODE" = true ]; then
+  password_masked=$(echo "${HELM_REPOSITORY_PASSWORD}" | sed 's/./*/g')
+
+  echo "-----------------------------------"
+  echo "HELM_REPOSITORY_URL: ${HELM_REPOSITORY_URL}"
+  echo "HELM_REPOSITORY_NAME: ${HELM_REPOSITORY_NAME}"
+  echo "HELM_REPOSITORY_USERNAME: ${HELM_REPOSITORY_USERNAME}"
+  echo "HELM_REPOSITORY_PASSWORD: ${password_masked}"
+  echo "HELM_BINARIES_URL: ${HELM_BINARIES_URL}"
+  echo "-----------------------------------"
+fi
+
+[ -z "$HELM_REPOSITORY_URL" ] && { echo "HELM_REPOSITORY_URL is required"; exit 1; }
+[ -z "$HELM_REPOSITORY_NAME" ] && { echo "HELM_REPOSITORY_NAME is required"; exit 1; }
+[ -z "$HELM_REPOSITORY_USERNAME" ] && { echo "HELM_REPOSITORY_USERNAME is required"; exit 1; }
+[ -z "$HELM_REPOSITORY_PASSWORD" ] && { echo "HELM_REPOSITORY_PASSWORD is required"; exit 1; }
+[ -z "$HELM_BINARIES_URL" ] && { echo "HELM_BINARIES_URL is required"; exit 1; }
+
+echo "Everything is silky smooth, well done!"
