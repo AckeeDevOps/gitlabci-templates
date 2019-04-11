c7dffb6c60b64ae75f37d7792c4c942c003f60cb
Štěpán Vraný <stepan.vrany@ackee.cz>
2019-04-11 10:31:20 +0200

    Update rclone-upload.sh

--- scripts/backend/nodejs/common/rclone-upload.sh
+++ scripts/backend/nodejs/common/rclone-upload.sh
@@ -9,14 +9,14 @@
 #   DOCS_GCS_PREFIX: a path where your files will be uploaded, should start with / e.g. /project-name/master/
 
 # Check required variables
-[ -z "${GCLOUD_SA_KEY}" ] && { echo "GCLOUD_SA_KEY is required"; exit 1; }
+[ -z "${DOCS_GCLOUD_SA_KEY}" ] && { echo "DOCS_GCLOUD_SA_KEY is required"; exit 1; }
 [ -z "${DOCS_GCLOUD_PROJECT_ID}" ] && { echo "DOCS_GCLOUD_PROJECT_ID is required"; exit 1; }
 [ -z "${DOCS_OUTPUT_DIRECTORY}" ] && { echo "DOCS_OUTPUT_DIRECTORY is required"; exit 1; }
 [ -z "${DOCS_GCS_BUCKET}" ] && { echo "DOCS_GCS_BUCKET is required"; exit 1; }
 [ -z "${DOCS_GCS_PREFIX}" ] && { echo "DOCS_GCS_PREFIX is required"; exit 1; }
 
 # create SA key file
-echo "${GCLOUD_SA_KEY}" | base64 -d > /tmp/key.json
+echo "${DOCS_GCLOUD_SA_KEY}" | base64 -d > /tmp/key.json
 
 # set rclone variables
 export RCLONE_GCS_PROJECT_NUMBER=${DOCS_GCLOUD_PROJECT_ID}
