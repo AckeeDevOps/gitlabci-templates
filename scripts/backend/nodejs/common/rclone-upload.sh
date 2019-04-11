b06352339013b66b874dafca3821e3622bd0cd28
Štěpán Vraný <stepan.vrany@ackee.cz>
2019-04-11 10:25:38 +0200

    Update rclone-upload.sh

--- scripts/backend/nodejs/common/rclone-upload.sh
+++ scripts/backend/nodejs/common/rclone-upload.sh
@@ -5,15 +5,15 @@
 #   GCLOUD_PROJECT_ID: gcloud project ID in plain text e.g. 'my-project-12345'
 #   DOCS_OUTPUT_DIRECTORY: directory with output HTML docs, relative or absolute path
 #     example: ./docs-output/.
-#   GCS_BUKET: name or GCS bucket e.g. my-bucket-name
-#   GCS_PREFIX: a path where your files will be uploaded, should start with / e.g. /project-name/master/
+#   DOCS_GCS_BUCKET: name or GCS bucket e.g. my-bucket-name
+#   DOCS_GCS_PREFIX: a path where your files will be uploaded, should start with / e.g. /project-name/master/
 
 # Check required variables
 [ -z "${GCLOUD_SA_KEY}" ] && { echo "GCLOUD_SA_KEY is required"; exit 1; }
 [ -z "${GCLOUD_PROJECT_ID}" ] && { echo "GCLOUD_PROJECT_ID is required"; exit 1; }
 [ -z "${DOCS_OUTPUT_DIRECTORY}" ] && { echo "AGLIO_DOCS_DIRECTORY is required"; exit 1; }
-[ -z "${GCS_BUKET}" ] && { echo "GCS_BUKET is required"; exit 1; }
-[ -z "${GCS_PREFIX}" ] && { echo "GCS_PREFIX is required"; exit 1; }
+[ -z "${DOCS_GCS_BUCKET}" ] && { echo "GCS_BUKET is required"; exit 1; }
+[ -z "${DOCS_GCS_PREFIX}" ] && { echo "GCS_PREFIX is required"; exit 1; }
 
 # create SA key file
 echo "${GCLOUD_SA_KEY}" | base64 -d > /tmp/key.json
@@ -26,8 +26,8 @@ export RCLONE_GCS_SERVICE_ACCOUNT_FILE=/tmp/key.json
 rclone config create remote gcs > /dev/null 2>&1
 
 # upload files to GCS
-rclone sync "${DOCS_OUTPUT_DIRECTORY}" remote:"${GCS_BUKET}${GCS_PREFIX}" > /dev/null 2>&1
+rclone sync "${DOCS_OUTPUT_DIRECTORY}" remote:"${DOCS_GCS_BUKET}${DOCS_GCS_PREFIX}" > /dev/null 2>&1
 
 # list files for logging purposes
 echo "Uploaded files:"
-rclone ls remote:"${GCS_BUKET}${GCS_PREFIX}"
+rclone ls remote:"${DOCS_GCS_BUKET}${DOCS_GCS_PREFIX}"
