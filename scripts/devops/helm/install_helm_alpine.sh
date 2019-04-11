9a494d39bcf7af1431dd9774009cc214e42dab56
Štěpán Vraný <stepan.vrany@ackee.cz>
2019-04-11 15:21:32 +0200

    Update install_helm_alpine.sh

--- scripts/devops/helm/install_helm_alpine.sh
+++ scripts/devops/helm/install_helm_alpine.sh
@@ -20,13 +20,3 @@ rm -rf linux-amd64 helm.tar.gz
 # Initialize helm without Tiller
 helm init --client-only > /dev/null 2>&1
 
-# Install Helm plugins
-helm plugin install https://github.com/chartmuseum/helm-push > /dev/null 2>&1
-
-# Add remote repository
-helm repo add \
-  --username "${HELM_REPOSITORY_USERNAME}" \
-  --password "${HELM_REPOSITORY_PASSWORD}" \
-  "${HELM_REPOSITORY_NAME}" \
-  "${HELM_REPOSITORY_URL}" > /dev/null 2>&1
-
