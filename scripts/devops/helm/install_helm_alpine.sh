c7e7941c3469fae4ff9a7fc63444716394d1bbd4
Tomáš Hejátko <tomas.hejatko@ackee.cz>
2020-05-28 15:00:09 +0200

    🔥 Remove helm init (removed from Helm3), remove cmd output silencing
    Related: #43533

--- scripts/devops/helm/install_helm_alpine.sh
+++ scripts/devops/helm/install_helm_alpine.sh
@@ -3,20 +3,16 @@
 echo "Now installing helm ..."
 
 # Add required packages
-apk add ca-certificates git > /dev/null 2>&1
+apk add ca-certificates git
 
 # Download Helm binaries
-wget "${HELM_BINARIES_URL}" -O helm.tar.gz > /dev/null 2>&1
+wget "${HELM_BINARIES_URL}" -O helm.tar.gz
 
 # Extract binaries
-tar -xvf helm.tar.gz > /dev/null 2>&1
+tar -xvf helm.tar.gz
 
 # Move helm to bin directory
 mv linux-amd64/helm /usr/local/bin
 
 # Clean temporary files
 rm -rf linux-amd64 helm.tar.gz
-
-# Initialize helm without Tiller
-helm init --client-only > /dev/null 2>&1
-
